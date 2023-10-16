eb4ace0984fcd920b2b6ceaeb4e5c8d3cc11fd7b
roland <146352884+rolandinthout@users.noreply.github.com>
2023-10-16 16:54:47 +0200

    Initial commit

---
+++ src/main/java/nl/miwnn/se12/roland/MovieDemo/MovieDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se12.roland.MovieDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se12/roland/MovieDemo/MovieDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se12.roland.MovieDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
